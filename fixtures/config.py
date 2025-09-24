4ab30a61ed89d6bc0c944944140eadfb3d69f6da
Mecit Tilim <mecittilim@gmail.com>
2025-09-25 01:32:27 +0300

    Add files via upload
    Test cases are created by Selenium.

---
+++ fixtures/config.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://useinsider.com/"
+CAREERS_URL = f"{BASE_URL}careers/"
+QA_URL = f"{BASE_URL}careers/quality-assurance/"
+LEVER_PAGE_URL = "https://jobs.lever.co/useinsider"
---
+++ pages/QA_page.py
@@ -0,0 +1,28 @@
+from pages.base_page import BasePage
+from locators.QA_locators import QALocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from fixtures import config
+from time import *
+
+class QAPage(BasePage):
+    def selectDDItem(self,DDLocatorType, DDLocatorValue, itemLocatorType,itemLocatorValue,item):
+
+        # Wait to end of AJAX actions
+        WebDriverWait(self.driver, 30).until(
+            lambda d: d.execute_script("return jQuery.active == 0")
+        )
+
+        # click drop-down
+        self.clickElement(DDLocatorType, DDLocatorValue, 10)
+
+        # wait to visibile of drop down items
+        self.waitElementToVisible(By.CSS_SELECTOR, QALocators.DROPDOWN_ITEMS_CSS)
+
+        # select item
+
+        dropDownList = self.driver.find_elements(itemLocatorType, itemLocatorValue)
+        for i in dropDownList:
+            if i.text == item:
+                i.click()
+                break
