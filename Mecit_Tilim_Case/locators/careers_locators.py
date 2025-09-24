f3c5f000c1391153b5947b980447f0e3739a3503
Mecit Tilim <mecittilim@gmail.com>
2025-09-25 00:11:50 +0300

    Delete Mecit_Tilim_Case directory

--- Mecit_Tilim_Case/locators/careers_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class CareersLocators:
-    LOCATIONS_BLOCK_ID = "career-our-location"
-    TEAMS_BLOCK_ID= "career-find-our-calling"
-    LIFE_BLOCK_XPATH = "//h2[text()='Life at Insider']"
